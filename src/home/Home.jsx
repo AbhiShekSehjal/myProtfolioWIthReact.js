6ad8ce822694cc18db79a9c625fc327ed7ca14f2
Abhishek <shek54112@gmail.com>
2025-04-01 13:55:26 +0530

    Initial commit

---
+++ src/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import Main from './components/main/Main'
+import Intro from './components/intro/Intro'
+
+function Home() {
+    return (
+        <>
+            <Navbar />
+            <Main />
+            <Intro />
+        </>
+    )
+}
+
+export default Home
---
+++ src/home/components/intro/Intro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Into.css"
+
+function Intro() {
+    return (
+        <div>
+            <h1>hello</h1>
+        </div>
+    )
+}
+
+export default Intro
