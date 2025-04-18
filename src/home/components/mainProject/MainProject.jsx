80bb2a191301a0768ad63ea651e9b7e0f28500ba
Abhishek <shek54112@gmail.com>
2025-04-18 09:17:35 +0530

    check responsive 3

--- src/home/components/mainProject/MainProject.jsx
+++ src/home/components/mainProject/MainProject.jsx
@@ -5,7 +5,7 @@ function MainProject() {
     return (
         <>
             <div className='MainProject'>My main project</div>
-            <div className="card" style={{ width: "50%", margin: "0 auto", background: "url(/images/rk.png)" }}>
+            <div className="card" style={{ width: "50%", margin: "0 auto", background: "url(/public/images/rk.png)" }}>
                 <a className="cardBtn" style={{ textDecoration: "none" }} >
                     <div>
                         <p>Show</p>
